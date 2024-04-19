09f0bcfcddc6d8e9cb4a502f3e412642b54f7c40
jifuwater <164806171+jifuwater@users.noreply.github.com>
2024-04-20 00:24:23 +0200

    Delete dotfiles/i3 directory

--- dotfiles/i3/scripts/miei/cpu.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-if [ $BLOCK_BUTTON == 1 ]; then
-	kitty -e htop
-fi
-
-echo ""$[100-$(vmstat 1 2|tail -1|awk '{print $15}')]"%"
--- dotfiles/i3/scripts/miei/meteo.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-meteo=$(curl wttr.in/Sedriano?format=1| xargs echo)
-if [ "$meteo" == "" ]; then
-	echo "  Off" 
-
-else
-	echo $meteo
-fi
-
