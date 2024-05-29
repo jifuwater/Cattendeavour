9f631e799d1428b8244b40daff95941d563b6e66
jifuwater <164806171+jifuwater@users.noreply.github.com>
2024-05-29 07:15:49 +0200

    Update meteo.sh

--- i3/scripts/miei/meteo.sh
+++ i3/scripts/miei/meteo.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
 meteo=$(curl wttr.in/Sedriano?format=1| xargs echo)
-if [ "$meteo" == "" ]; then
+first="${meteo%% *}"
+if [ "$meteo" == "" ] || [ "$first" == "Unknown" ]; then
 	echo "  Off" 
 
 else
 	echo $meteo
 fi
-
