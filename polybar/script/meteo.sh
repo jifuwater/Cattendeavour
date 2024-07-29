78e5a8939b10e87da1c53fd42021a4da779fbfb7
jifuwater <164806171+jifuwater@users.noreply.github.com>
2024-07-29 12:38:56 +0200

    Delete polybar directory

--- polybar/script/meteo.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-meteo=$(curl -s  wttr.in/Sedriano?format=1)
-first="${meteo%% *}"
-if [ "$meteo" == "" ] || [ "$first" == "Unknown" ]; then
-	echo "  Off" 
-
-else
-	echo $meteo
-fi
