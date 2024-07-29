18fc85eb11bf909979334d98aa6f41d1b55dd2d6
jifuwater <164806171+jifuwater@users.noreply.github.com>
2024-07-29 12:21:29 +0200

    Add files via upload

---
+++ polybar/script/wallpaperun.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+w_path=~/.config/wallpaper/
+c_path=~/.config/wallpaper/current
+config=~/.config/
+current=$(cat $c_path)
+
+
+new=$((current - 1))
+if [ $new == "-1" ]; then
+	new=12
+fi
+
+rm "${config}sfondo.jpg"
+cp "$w_path$new.jpg" $config
+mv "$config$new.jpg" "${config}sfondo.jpg"
+sed -i '1d' $c_path 
+echo $new >> $c_path
+feh --bg-fill ~/.config/sfondo.jpg
